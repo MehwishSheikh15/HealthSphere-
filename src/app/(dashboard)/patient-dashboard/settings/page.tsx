d158d40d4fc3300fee4b453985acc8534f46564b
Mehwish Sheikh <mehwishsheikh451sheikh@gmail.com>
2025-10-18 13:50:07 +0000

    Try fixing this error: `Runtime Error: Error: CardFooter is not defined.

--- src/app/(dashboard)/patient-dashboard/settings/page.tsx
+++ src/app/(dashboard)/patient-dashboard/settings/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
 import { Switch } from "@/components/ui/switch";
 import { Button } from "@/components/ui/button";
