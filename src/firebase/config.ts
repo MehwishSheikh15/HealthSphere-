2663804d0bad05e96bf0e58396ba308077b671a3
Mehwish Sheikh <mehwishsheikh451sheikh@gmail.com>
2025-10-18 09:37:39 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8048832737-944f4",
+  "appId": "1:37731812227:web:3b438c2fcee0d5bf856cb6",
+  "apiKey": "AIzaSyA_VzzXaFXKcDQbkdZjcia_8gyxzrqBTzo",
+  "authDomain": "studio-8048832737-944f4.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "37731812227"
+};
