4d5bb1c5156a2cec47021c2ef5ac942d9767ad32
Mehwish Sheikh <160774324+MehwishSheikh15@users.noreply.github.com>
2025-11-19 19:25:27 +0500

    Update page.tsx

--- src/app/auth/login/patient/page.tsx
+++ src/app/auth/login/patient/page.tsx
@@ -68,12 +68,12 @@ export default function PatientLoginPage() {
         <div className="grid gap-2">
           <div className="flex items-center">
             <Label htmlFor="password">Password</Label>
-            <Link
+            {/* <Link
               href="/auth/forgot-password"
               className="ml-auto inline-block text-sm underline"
             >
               Forgot your password?
-            </Link>
+            </Link> */}
           </div>
           <Input 
             id="password" 
