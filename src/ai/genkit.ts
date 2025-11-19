e9b61820591cd2495fdcb9cc756fff9c46cca057
Mehwish Sheikh <160774324+MehwishSheikh15@users.noreply.github.com>
2025-11-19 15:31:25 +0500

    Update genkit.ts

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,30 +1,3 @@
-// import {genkit} from 'genkit';
-// import {googleAI} from '@genkit-ai/google-genai';
-
-// export const ai = genkit({
-//   plugins: [googleAI()],
-//   model: 'googleai/gemini-2.5-flash',
-// });
-// import { genkit } from "genkit";
-// import { googleAI } from "@genkit-ai/google-genai";
-
-// // REGISTER ALL FLOWS
-// import "@/ai/flows/first-aid-flow";
-// import "@/ai/flows/skin-analysis-flow";
-// import "@/ai/flows/medicine-check-flow";
-// import "@/ai/flows/lab-report-summary-flow";
-// import "@/ai/flows/psychologist-chat-flow";
-
-// export const ai = genkit({
-//   plugins: [
-//     googleAI({
-//       apiKey: process.env.GEMINI_API_KEY,
-//     }),
-//   ],
-//   model: "googleai/gemini-2.5-flash",
-// });
-
-
 import { genkit } from "genkit";
 import { googleAI } from "@genkit-ai/google-genai";
 
